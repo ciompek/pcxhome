339b1796fcb0e4f4e0eabe7c7fe75284425fe9b4
ciompek <ciompek@gmail.com>
2018-04-23 21:41:12 +0200

    0.1

--- project/__init__.py
+++ project/__init__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
+import config as cfg
 #===============================================
 # configuration part for the whole project
 #===============================================
-__version__ = '0.1'
+__version__ = cfg.app['version']
 app = Flask('project')
-app.config['SECRET_KEY'] = '8WEAy3VyhxXiviiAlklghk6N7jWnqoFptd8NvRs3lXE' # some salt we added to the cookies ;)
-app.debug = False # Enable / Disable debuging of the app
+app.config['SECRET_KEY'] = cfg.app['SECRET_KEY'] # some salt we added to the cookies ;)
+app.debug = cfg.app['debug'] # Enable / Disable debuging of the app
+
 toolbar = DebugToolbarExtension(app)
 from project.controllers import * #import of all the controllers in the project
--- runserver.py
+++ runserver.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 import os
 from project import app
+from project import config as cfg
+
+#if __name__ == '__main__':
+#    port = int(os.environ.get("PORT", 8080))
+#    app.run('0.0.0.0', port=port, debug=False)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run('0.0.0.0', port=port, debug=False)
+	port = int(os.environ.get("PORT", cfg.server['port']))
+	app.run('0.0.0.0', port=port, debug=cfg.server['debug'])
